b2db9ac1153ded6e634664126576581d1a9838fa
savannahvaith <sav.vaithilingam@gmail.com>
2021-07-08 15:33:37 +0100

    SV: Initial Structure

---
+++ src/main/java/com/qa/springfootball/SpringFootballApplication.java
@@ -0,0 +1,13 @@
+package com.qa.springfootball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFootballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFootballApplication.class, args);
+	}
+
+}
